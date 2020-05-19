9704b9998f5920118b7b72f46e091209ae198abb
Stefan Tafara <tafara.stefan@gmail.com>
2020-05-20 00:45:28 +0300

    fixed token problems

--- bot.py
+++ bot.py
@@ -1,13 +1,24 @@
 import telebot
 import config
 
+# reading token from config...
+print(f'reading token from config...')
 token = config.token
+print(f'token: {token}')
+# starting bot...
+print('starting bot...')
+bot = telebot.TeleBot(token)
+print('bot started.')
 
-bot = telebot.TeleBot()
 
 @bot.message_handler()
 def handle_message(message):
-    print(message.text)
+    # receiving message
+    print(f'message received: {message.text}')
+    # reply
+    print(f'sending reply...')
     bot.send_message(chat_id=message.chat_id, text='Hi there')
+    print(f'reply sent')
+
 
 bot.polling(none_stop=True, timeout=123)
