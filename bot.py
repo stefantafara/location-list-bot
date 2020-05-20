bbb64883e4a574b1ac96d220e1a46049a53df276
Stefan Tafara <tafara.stefan@gmail.com>
2020-05-20 23:29:45 +0300

    added redis and some commands

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import telebot, os
+import telebot, os, redis, logging
 
 # there is no need in config file. use Heroku config
 # import config
@@ -13,6 +13,12 @@ print('starting bot...')
 bot = telebot.TeleBot(token)
 print('bot started.')
 
+# set logger
+logger = telebot.logger
+telebot.logger.setLevel(logging.DEBUG)
+
+# Redis
+r = redis.from_url(os.environ.get("REDIS_URL"))
 
 @bot.message_handler()
 def handle_message(message):
@@ -20,10 +26,46 @@ def handle_message(message):
     print(f'message received: {message.text}')
     # reply
     print(f'sending reply...')
-    bot.send_message(chat_id=message.chat.id, text='Hi there')
+    bot.send_message(chat_id=message.chat.id, text='Hi!')
     bot.send_message(chat_id=message.chat.id, text=f'Your text was: {message.text}')
-    bot.send_message(chat_id=message.chat.id, text=f'Мы с Жаки полуночные девелоперы')
     print(f'reply sent')
+
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am Location List Bot.
+PLease use these commands:\
+/help - for help\
+/add - to add new location\
+/list - to watch list of 10 last locations\
+/reset - to reset all locations\
+""")
+
+
+@bot.message_handler(commands=['/add'])
+def add_location(message):
+    print('requesting location...')
+    bot.send_message(chat_id=message.chat.id, text='Please input location')
+    print('pushing location to Redis...')
+    r.lpush(message.chat.id, message.text)
+    print('pushed successfully')
+    bot.send_message(chat_id=message.chat.id, text='Location was successfully added')
+
+
+@bot.message_handler(commands=['/list'])
+def list_locations(message):
+    bot.send_message(chat_id=message.chat.id, text='Please input location')
+    for location in r.lrange(message.chat.id, 0, 9):
+        print(f'location: {location}')
+        bot.send_message(chat_id=message.chat.id, text=location)
+
+
+@bot.message_handler(commands=['/reset'])
+def send_welcome(message):
+    print(f'deleting all locations with ID: {message.chat.id}')
+    r.del(message.chat.id)
+    bot.send_message(chat_id=message.chat.id, text='All locations were deleted')
 
 
 if __name__ == '__main__':
