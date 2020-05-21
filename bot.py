8868f6b2c2c0a618ee99d2a9fbfca3c9d9609352
Stefan Tafara <tafara.stefan@gmail.com>
2020-05-21 21:36:35 +0300

    added to messages: reply_markup=create_keyboard()

--- bot.py
+++ bot.py
@@ -52,52 +52,52 @@ def handle_message(message):
     # reply
     print(f'sending reply...')
     bot.send_message(chat_id=message.chat.id, text='Hi!')
-    bot.send_message(chat_id=message.chat.id, text=f'Your text was: {message.text}')
+    bot.send_message(chat_id=message.chat.id, text=f'Your text was: {message.text}', reply_markup=create_keyboard())
     print(f'reply sent')
 
 
 @bot.message_handler(commands=['help', 'start'])
 def help(message):
-    bot.reply_to(message, """\
+    bot.send_message(chat_id=message.chat.id, text="""
 Hi there, I am Location List Bot.
 PLease use these commands:
 /help - for help
 /add - to add new location
 /list - to watch list of 10 last locations
 /reset - to reset all locations
-""")
+""", reply_markup=create_keyboard())
 
 
 @bot.message_handler(commands=['add'])
 def command_add(message):
     print('requesting location...')
     bot.send_message(chat_id=message.chat.id, text='Please input location')
-    bot.register_next_step_handler(message, add_location)
+    bot.register_next_step_handler(message, add_location, reply_markup=create_keyboard())
 
 
 def add_location(message):
     print('pushing location to Redis...')
     r.lpush(message.chat.id, message.text)
     print('pushed successfully')
-    bot.send_message(chat_id=message.chat.id, text='Thank you! Location was successfully added.')
+    bot.send_message(chat_id=message.chat.id, text='Thank you! Location was successfully added.', reply_markup=create_keyboard())
 
 
 @bot.message_handler(commands=['list'])
 def list_locations(message):
     locations = r.lrange(message.chat.id, 0, 9)
     if len(locations) == 0:
-        bot.send_message(chat_id=message.chat.id, text='Sorry your locations list is empty')
+        bot.send_message(chat_id=message.chat.id, text='Sorry your locations list is empty', reply_markup=create_keyboard())
     else:
         for location in locations:
             print(f'location: {location}')
-            bot.send_message(chat_id=message.chat.id, text=location)
+            bot.send_message(chat_id=message.chat.id, text=location, reply_markup=create_keyboard())
 
 
 @bot.message_handler(commands=['reset'])
 def reset(message):
     print(f'deleting all locations with ID: {message.chat.id}')
     r.delete(message.chat.id)
-    bot.send_message(chat_id=message.chat.id, text='All locations were deleted')
+    bot.send_message(chat_id=message.chat.id, text='All locations were deleted', reply_markup=create_keyboard())
 
 
 if __name__ == '__main__':
